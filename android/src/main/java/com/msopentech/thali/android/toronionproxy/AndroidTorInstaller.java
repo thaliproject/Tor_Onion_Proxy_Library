5b97cf63f210da45cb51a599eebaafc32d6b87fd
sisbell <shane.isbell@gmail.com>
2019-01-28 12:59:19 -0800

    Fixes #46: update tor-android-binary available (0.3.5.6-rc). Fixes #61: Remove the default torrc file in Android project. Fixes #55: Ability to Add a custom torrc file to the AndroidTorInstaller

--- android/src/main/java/com/msopentech/thali/android/toronionproxy/AndroidTorInstaller.java
+++ android/src/main/java/com/msopentech/thali/android/toronionproxy/AndroidTorInstaller.java
@@ -21,44 +21,35 @@ import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.concurrent.TimeoutException;
 
-import static org.torproject.android.binary.TorResourceInstaller.streamToFile;
 import static org.torproject.android.binary.TorServiceConstants.TORRC_ASSET_KEY;
 
 /**
  * Installs Tor for an Android app. This is a wrapper around the <code>TorResourceInstaller</code>.
- *
- * This installer will override the torrc file that is provided by <code>org.torproject.android.binary</code>
- * with the torrc in the toronionproxy assets folder.
  */
 public class AndroidTorInstaller extends TorInstaller {
 
     private final TorResourceInstaller resourceInstaller;
-
-    private final Context context;
 
     private final File installDir;
 
     private static final String TAG = "TorInstaller";
 
     public AndroidTorInstaller(Context context, File installDir) {
-        this.context = context;
         this.installDir = installDir;
         this.resourceInstaller = new TorResourceInstaller(context, installDir);
+    }
+
+    public void updateTorConfigCustom(String content) throws IOException, TimeoutException {
+        File outFile = new File(installDir, TORRC_ASSET_KEY);
+        resourceInstaller.updateTorConfigCustom(outFile, content);
     }
 
     @Override
     public boolean setup() throws IOException {
         try {
-            if (resourceInstaller.installResources()) {
-                InputStream is = context.getAssets().open(TORRC_ASSET_KEY);
-                File outFile = new File(installDir, TORRC_ASSET_KEY);
-                return streamToFile(is, outFile, false, false);
-            }
-            Log.w(TAG, "Failed to setup tor");
-            return false;
+            return resourceInstaller.installResources();
         } catch (TimeoutException e) {
             Log.w(TAG, "Failed to setup tor: " + e.getMessage());
             throw new IOException(e);
