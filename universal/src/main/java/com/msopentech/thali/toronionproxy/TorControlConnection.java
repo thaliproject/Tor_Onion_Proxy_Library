cdf9a523e47a23259366c0e34b54fcdac947acdf
sisbell <shane.isbell@gmail.com>
2019-04-25 18:49:18 -0700

    Fixes #59: Take Ownership Feature

---
+++ universal/src/main/java/com/msopentech/thali/toronionproxy/TorControlConnection.java
@@ -0,0 +1,27 @@
+package com.msopentech.thali.toronionproxy;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Collections;
+
+public final class TorControlConnection extends net.freehaven.tor.control.TorControlConnection {
+    public TorControlConnection(Socket socket) throws IOException {
+        super(socket);
+    }
+
+    public TorControlConnection(InputStream inputStream, OutputStream outputStream) {
+        super(inputStream, outputStream);
+    }
+
+    public TorControlConnection(Reader reader, Writer writer) {
+        super(reader, writer);
+    }
+    
+    public void takeownership() throws IOException {
+        sendAndWaitForResponse("TAKEOWNERSHIP\r\n", (String)null);
+    }
+
+    public void resetOwningControllerProcess() throws IOException {
+        resetConf(Collections.singleton("__OwningControllerProcess"));
+    }
+}
